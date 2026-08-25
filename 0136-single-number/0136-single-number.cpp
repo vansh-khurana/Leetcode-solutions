7379bfc80dbd3a63caa5dac3fd772d4e67e95c77
vansh-khurana <vanshkhurana890@gmail.com>
2026-08-25 19:48:02 +0530

    Time: 0 ms (100%), Space: 20.5 MB (84.07%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& arr) {
-        unordered_map<int, int> hash;
-
-        for(int i = 0; i < arr.size(); i++) {
-            hash[arr[i]]++;
-        }
-
-        for(int i = 0; i < arr.size(); i++) {
-            if(hash[arr[i]] == 1) {
-                return arr[i];
-            }
-        }
-
-        return -1;
+     int xorr=0;
+     for(int i =0;i<arr.size();i++){
+        xorr=xorr^arr[i];
+     }
+     return xorr;
     }
 };
